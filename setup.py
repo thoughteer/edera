fd5fc24554b932217c5280fca6ab666d97433b2e
Iskander Sitdikov <thoughteer@joom.com>
2020-06-03 07:51:15 +0300

    Bump the version up

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name="edera",
-    version="0.12",
+    version="0.12.1",
     description="A distributed workflow management framework",
     classifiers=[
         "Development Status :: 4 - Beta",
